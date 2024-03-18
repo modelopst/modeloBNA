8ca18b24e1d84d4f4cdae6a3700209a1d34d8aaa
rbarragan14 <ricardobarragano14@gmail.com>
2024-03-18 16:21:02 -0500

    Primer commit

---
+++ model_definitions/dfd4052e-f91b-4aa5-9c79-f26d649dd931/model_modules/scoring.py
@@ -0,0 +1,114 @@
+
+#from sqlalchemy import VARCHAR
+import teradataml
+from teradataml import copy_to_sql, DataFrame
+from teradataml import create_context, get_context, remove_context, execute_sql
+
+from aoa import (
+    record_training_stats,
+    save_plot,
+    aoa_create_context,
+    ModelContext
+)
+
+
+
+import joblib
+import pandas as pd
+from collections import OrderedDict
+
+from teradatasqlalchemy import FLOAT, INTEGER
+
+#from teradatasqlalchemy import FLOAT, INTEGER
+
+def score(context: ModelContext, **kwargs):
+
+    aoa_create_context()
+
+    #model = joblib.load(f"{context.artifact_input_path}/model.joblib")
+
+    #feature_names = context.dataset_info.feature_names
+    #target_name = context.dataset_info.target_names[0]
+    #entity_key = context.dataset_info.entity_key
+
+    #features_tdf = DataFrame.from_query(context.dataset_info.sql)
+    #features_pdf = features_tdf.to_pandas(all_rows=True)
+
+    print("Scoring")
+    #predictions_pdf = model.predict(features_pdf[feature_names])
+
+    print("Finished Scoring")
+
+    # store the predictions
+    #predictions_pdf = pd.DataFrame(predictions_pdf, columns=[target_name])
+    #predictions_pdf[entity_key] = features_pdf.index.values
+    # add job_id column so we know which execution this is from if appended to predictions table
+    #predictions_pdf["job_id"] = context.job_id
+
+    # teradataml doesn't match column names on append.. and so to match / use same table schema as for byom predict
+    
+    #predictions_pdf["json_report"] = ""
+    #predictions_pdf = predictions_pdf[["job_id", entity_key, target_name, "json_report"]]
+
+    
+ 
+    execute_sql("SET SESSION SEARCHUIFDBPATH = pocanalytics;")
+    execute_sql("database pocanalytics;")
+    
+    
+    print("Inicia Consulta 3")
+    
+    df = DataFrame.from_query("SELECT ROW_NUMBER() OVER (ORDER BY NR_TLFN,ID_LNHA,NR_CPF,NR_CPF_NUM,DS_CRCT_PLNO ) AS Id, "
+                          "a.* FROM vivoaltovalor a")
+    
+    sto = teradataml.Script(data=df,
+                        script_name='VIVO_AltoValorSTO.py',
+                        script_command=f'tdpython3 ./pocanalytics/VIVO_AltoValorSTO.py',
+                        data_order_column="Id",
+                        is_local_order=True,
+                        delimiter='\t',
+                        returns=OrderedDict([("Id", INTEGER()),("Score", FLOAT())])) 
+    
+    sto.execute_script()
+    
+    
+    print("Fin sto")
+    df1 = df.merge(right = sto.result, on = ["Id"], lsuffix = "t1", rsuffix = "t2")
+    df2=df1.assign(drop_columns=False, Id = df1.t1_Id)
+    df3=df2.drop(['t1_Id', 't2_Id'], axis=1)
+    
+  
+    
+    df3.to_sql('vivoaltovalor_score', primary_index="Id", if_exists="replace")
+    
+    
+    print("Fin STO")
+
+
+    #copy_to_sql(df=predictions_pdf,
+    #            schema_name=context.dataset_info.predictions_database,
+    #            table_name=context.dataset_info.predictions_table,
+    #            index=False,
+    #          if_exists="append")
+
+    print("Saved predictions in Teradata")
+
+    # calculate stats
+    #predictions_df = DataFrame.from_query(f"""
+    #    SELECT 
+    #        * 
+    #    FROM {context.dataset_info.get_predictions_metadata_fqtn()} 
+    #        WHERE job_id = '{context.job_id}'
+    #""")
+
+    #record_scoring_stats(features_df=features_tdf, predicted_df=predictions_df, context=context)
+
+
+# Add code required for RESTful API
+class ModelScorer(object):
+
+    def __init__(self):
+        self.model = joblib.load("artifacts/input/model.joblib")
+
+    def predict(self, data):
+        return self.model.predict(data)
---
+++ model_definitions/dfd4052e-f91b-4aa5-9c79-f26d649dd931/model_modules/training.py
@@ -0,0 +1,110 @@
+import teradataml
+from teradataml import DataFrame
+from teradataml import *
+from teradataml import create_context, get_context, remove_context, execute_sql
+
+from aoa import (
+    record_training_stats,
+    save_plot,
+    aoa_create_context,
+    ModelContext
+)
+
+import joblib
+
+def plot_feature_importance(fi, img_filename):
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    feat_importances = pd.Series(fi)
+    feat_importances.nlargest(10).plot(kind='barh').set_title('Feature Importance')
+    fig = plt.gcf()
+    fig.savefig(img_filename, dpi=500)
+    plt.clf()
+    
+
+def train(context: ModelContext, **kwargs):
+    aoa_create_context()
+
+    # ULTIMO   feature_names = context.dataset_info.feature_names
+    # ULTIMO   target_name = context.dataset_info.target_names[0]
+
+    # read training dataset from Teradata and convert to pandas
+    # ULTIMO   train_df = DataFrame.from_query(context.dataset_info.sql)
+    # ULTIMO   train_pdf = train_df.to_pandas(all_rows=True)
+
+    # split data into X and y
+    # ULTIMO   X_train = train_pdf[feature_names]
+    # ULTIMO   y_train = train_pdf[target_name]
+    
+    
+    print(os.environ["AOA_CONN_USERNAME"])
+    print("Starting training...")
+    
+    #qry1 = "SET SESSION SEARCHUIFDBPATH = " +  os.environ["AOA_CONN_USERNAME"] + ";"
+    #print (qry1)
+    
+    #execute_sql(qry1)
+    
+    print(os.environ["AOA_CONN_DATABASE"])
+
+    # fit model to training data
+    #model = Pipeline([('scaler', MinMaxScaler()),
+    #                  ('xgb', XGBClassifier(eta=context.hyperparams["eta"],
+    #                                        max_depth=context.hyperparams["max_depth"]))])
+
+    #model.fit(X_train, y_train)
+
+    print("Finished training") 
+    # OJO ESTE ES DE PRUEBA print (context.dataset_info.username)
+
+    #print(context.dataset_info.predictions_database)
+    
+    print("Inicia Install .......")
+    
+    #install_file(file_identifier='VIVO_AltoValorSTO', file_path=f"./VIVO_AltoValorSTO.py", 
+    #         is_binary=False)
+    
+    #execute_sql("call SYSUIF.INSTALL_FILE('stoSalesForecastnew', 'stoSalesForecastnew.py', 'cz!./stoSalesForecastnew.py');")
+    
+  
+    # export model artefacts
+    #joblib.dump(model, f"{context.artifact_output_path}/model.joblib")
+
+    # we can also save as pmml so it can be used for In-Vantage scoring etc.
+    #xgboost_to_pmml(pipeline=model, col_names=feature_names, target_name=target_name,
+    #                pmml_f_name=f"{context.artifact_output_path}/model.pmml")
+
+    print("Saved trained model")
+
+    #from xgboost import plot_importance
+    #model["xgb"].get_booster().feature_names = feature_names
+    
+    #plot_importance(model["xgb"].get_booster(), max_num_features=10)
+    save_plot("feature_importance.png", context=context)
+
+    #feature_importance = model["xgb"].get_booster().get_score(importance_type="weight")
+    
+    print("Inicia Nuevo")
+    
+    #model_pdf = model.result.to_pandas()[['Id','Score']]
+    #predictor_dict = {}
+    
+    #for index, row in model_pdf.iterrows():
+    #    if row['predictor'] in feature_names:
+    #        value = row['estimate']
+    #        predictor_dict[row['predictor']] = value
+
+    #feature_importance = dict(sorted(predictor_dict.items(), key=lambda x: x[1], reverse=True))
+    #keys, values = zip(*feature_importance.items())
+    #norm_values = (values-np.min(values))/(np.max(values)-np.min(values))
+    #feature_importance = {keys[i]: float(norm_values[i]*1000) for i in range(len(keys))}
+    #plot_feature_importance(feature_importance, f"{context.artifact_output_path}/feature_importance")
+
+
+    
+    #record_training_stats(train_df,
+    #                      features=feature_names,
+    #                      targets=[target_name],
+    #                      categorical=[target_name],
+    #                      feature_importance=feature_importance,
+    #                      context=context)
